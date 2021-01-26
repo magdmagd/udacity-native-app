5a9c23f95ecca1ebffb0df123e77d491a5f6f309
magdmagd <dv.aboelmagd@gmail.com>
2021-01-26 15:46:04 +0200

    fix some errors

--- utils/_calendar.js
+++ utils/_calendar.js
@@ -1,14 +1,14 @@
+import { AsyncStorage } from 'react-native';
+import { getMetricMetaInfo, timeToString } from './helpers';
 
-import { AsyncStorage } from 'react-native'
-import { getMetricMetaInfo, timeToString } from './helpers'
-
-export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
+export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar';
 
 function getRandomNumber (max) {
   return Math.floor(Math.random() * max) + 0
 }
 
-function setDummyData () {
+function setDummyData ()
+{
   const { run, bike, swim, sleep, eat } = getMetricMetaInfo()
 
   let dummyData = {}
@@ -31,9 +31,10 @@ function setDummyData () {
   AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(dummyData))
 
   return dummyData
-}
+}//end function setDummyData ()
 
-function setMissingDates (dates) {
+function setMissingDates (dates) 
+{
   const length = Object.keys(dates).length
   const timestamp = Date.now()
 
@@ -47,65 +48,13 @@ function setMissingDates (dates) {
   }
 
   return dates
-}
+}//end setMissingDates (dates)
 
-export function formatCalendarResults (results) {
+export function formatCalendarResults (results) 
+{
   return results === null
     ? setDummyData()
     : setMissingDates(JSON.parse(results))
-} 
-import { AsyncStorage } from 'react-native'
-import { getMetricMetaInfo, timeToString } from './helpers'
+}//end formatCalendarResults (results) 
 
-export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
 
-function getRandomNumber (max) {
-  return Math.floor(Math.random() * max) + 0
-}
-
-function setDummyData () {
-  const { run, bike, swim, sleep, eat } = getMetricMetaInfo()
-
-  let dummyData = {}
-  const timestamp = Date.now()
-
-  for (let i = -183; i < 0; i++) {
-    const time = timestamp + i * 24 * 60 * 60 * 1000
-    const strTime = timeToString(time)
-    dummyData[strTime] = getRandomNumber(3) % 2 === 0
-      ? {
-          run: getRandomNumber(run.max),
-          bike: getRandomNumber(bike.max),
-          swim: getRandomNumber(swim.max),
-          sleep: getRandomNumber(sleep.max),
-          eat: getRandomNumber(eat.max),
-        }
-      : null
-  }
-
-  AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(dummyData))
-
-  return dummyData
-}
-
-function setMissingDates (dates) {
-  const length = Object.keys(dates).length
-  const timestamp = Date.now()
-
-  for (let i = -183; i < 0; i++) {
-    const time = timestamp + i * 24 * 60 * 60 * 1000
-    const strTime = timeToString(time)
-
-    if (typeof dates[strTime] === 'undefined') {
-      dates[strTime] = null
-    }
-  }
-
-  return dates
-}
-
-export function formatCalendarResults (results) {
-  return results === null
-    ? setDummyData()
-    : setMissingDates(JSON.parse(results))
-} 
